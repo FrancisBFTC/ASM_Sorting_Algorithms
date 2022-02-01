2af8a4bc7994dde9016eea05cc65d1d2b0fa5e86
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-01-31 21:45:44 -0300

    Algoritmo de Inserção em Assembly

--- InsertionSort.asm
+++ InsertionSort.asm
@@ -12,44 +12,52 @@
 ; o próximo item da sequência e colocá-lo no local apropriado de acordo com o critério de ordenação.
 
 
-Vector 	dd 	8, 5, 9, 1, 3, 2, 0, 4, 7, 6    ; Vetor de Exemplo
 i dd 1
 j dd 0
 x dd 0
 
+; --------------------------------------------------------------------------------------------------------------------------
+; ARGUMENTOS DA ROTINA INSERTIONSORT ---------------------------------------------------------------------------------------
 ; IN:  ECX = Tamanho do Vetor
 ;      ESI = Endereço do Vetor
 ;
 ; OUT: Nenhum.
-InsertionSort:
-	pushad
-	loop_for:
-		mov 	ebx, dword[i]
-		shl 	ebx, 2
-		mov 	eax, dword[esi + ebx]
-		mov 	dword[x], eax
-		mov 	ebx, dword[i]
-		dec 	ebx
-		mov 	dword[j], ebx
-		mov 	dword[esi-4], eax
-	init_while:
-		mov 	ebx, dword[j]
-		shl 	ebx, 2
-		mov 	eax, dword[esi + ebx]
-		cmp 	dword[x], eax
-		jb 		loop_while
-		jmp 	return_for
-	loop_while:
-		mov 	dword[esi+ebx+4], eax
-		dec 	dword[j]		
-		jmp 	init_while
-	return_for:
-		mov 	eax, dword[x]
-		mov 	dword[esi+ebx+4], eax
-		inc 	dword[i]
-		loop 	loop_for
-	popad
-ret
+InsertionSort:                             ; Label que será chamada por instrução CALL
+	pushad                                 ; Armazene todos os registradores na pilha
+	mov 	dword[i], 1                    ; Inicializa a variável i com 1
+	mov 	dword[j], 0                    ; Inicializa a variável j com 0
+	mov 	dword[x], 0                    ; Inicializa a variável x com 0
+	dec 	ecx                            ; ecx - 1 (tamanho - 1), porque i começa com 1
+	loop_for_Ins:                          ; Inicio do loop FOR -> for(...){
+		mov 	ebx, dword[i]              ; Mova o valor de i para ebx
+		shl 	ebx, 2                     ; Multiplique rápidamente ebx por 4 (4 bytes = 32 bits)
+		mov 	eax, dword[esi + ebx]      ; Acesse o índice i no Vetor através de esi + ebx ou Vector[i], mova para eax
+		mov 	dword[x], eax              ; Mova o conteúdo de eax para a variável x
+		mov 	ebx, dword[i]              ; Mova o índice i para ebx
+		dec 	ebx                        ; Subtraia ebx - 1, ou seja, i - 1
+		mov 	dword[j], ebx              ; Mova ebx (i - 1) para j, que equivale a j = i - 1
+		mov 	dword[esi-4], eax          ; Mova eax (Valor de x) para esi-4 (4 bytes), que seria Vector[-1] = x
+	loop_while_Ins:                        ; Inicio do loop WHILE para comparação -> while(...){
+		mov 	ebx, dword[j]              ; mova o valor de j (i - 1) para ebx
+		shl 	ebx, 2                     ; Multiplique rápidamente ebx por 4 (4 bytes = 32 bits)
+		mov 	eax, dword[esi + ebx]      ; Acesse o índice j no Vetor através de esi + ebx ou Vector[j], mova para eax 
+		cmp 	dword[x], eax              ; Compare x com o conteúdo de Vector[j] (eax)
+		jb 		return_while_Ins           ; Se for menor, salte para o retorno do loop WHILE
+		jmp 	return_for_Ins             ; Se não for, então salte para o retorno do loop FOR
+	return_while_Ins:                      ; Retorno do loop WHILE com procedimentos
+		mov 	dword[esi+ebx+4], eax      ; Mova o conteudo de Vector[j] para Vector[j+1] -> esi+ebx+4 (4 bytes = 1 int)
+		dec 	dword[j]                   ; Decremente j, obs.: Se j for -1, em Asm ele sera 0xFFFFFFFF
+		jmp 	loop_while_Ins             ; Volte para o inicio do loop WHILE para comparação
+	return_for_Ins:                        ; Retorno do loop FOR com procedimentos
+		mov 	eax, dword[x]              ; Mova o valor de x para eax
+		mov 	dword[esi+ebx+4], eax      ; Mova x para Vector[j+1]
+		inc 	dword[i]                   ; Incremente o índice i
+		cmp 	dword[i], ecx              ; Compare índice i com o tamanho do vetor em ecx
+		jbe 	loop_for_Ins               ; Se for menor ou igual, Volte para o laço externo FOR
+	popad                                  ; Restaure todos os registradores da pilha
+ret                                        ; Retorne para a chamada da instrução CALL
+; --------------------------------------------------------------------------------------------------------------------------
+
 
 ; EQUIVALENTE LINGUAGEM C
 ;void InsertionSort (int vector[], int size){
